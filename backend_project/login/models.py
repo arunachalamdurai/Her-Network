658856da9928890ab59cef99fb396c440027fcc9
Vishwa <vishwa@v.com>
2024-10-15 22:24:28 +0530

    commit 2

--- backend_project/login/models.py
+++ backend_project/login/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 
 # Create your models here.
-
+class Login_detail(models.Model):
+    name = models.CharField(max_length=20)
+    email = models.EmailField(max_length=254)
+    role = models.CharField(max_length=10)
+    skill = models.CharField(max_length=10)
+    password = models.CharField( max_length=50,)
 
 
 
--- backend_project/login/views.py
+++ backend_project/login/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
 from django.forms import Form
-
+from .models import Login_detail
 
 
 
 # Create your views here.
 def loginhome(request):
-    if request.method=='POST':
-        print(request.POST)
+    def post(request,self):
+        if (request.method=='POST'):
+            login_detail= Login_detail()
+            login_detail.name=request.POST['Uname']
+            login_detail.email=request.POST['Uemail']
+            login_detail.role=request.POST['role']
+            login_detail.skill=request.POST.getlist('skills')
+            login_detail.password=request.POST['Upsw']
+            print("sssss",login_detail.save())
+    post(request,self=__name__)   
     return render(request,'login.html')
  
-# Create your views here.
+
