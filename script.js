21031ce0a3bf9c6b42fedb3c7eb2bd9c9e8b214b
arunachalamdurai <arundurai158@gmail.com>
2024-10-15 14:06:48 +0530

    script file include

--- script.js
+++ script.js
@@ -9,3 +9,27 @@ function showSkillsInput() {
         skillsContainer.style.display = 'none';
     }
 }
+
+function showResister() {
+    const regi = document.getElementById('regi');
+    const log = document.getElementById('log');
+
+        regi.style.display = 'block';        
+        log.style.display = 'none';
+
+}
+
+
+
+document.getElementById("show-password").addEventListener("click", showPassword);
+
+function showPassword() {
+    const passField = document.getElementById("Upsw");
+    
+    // Toggle the type between "password" and "text"
+    if (passField.type === "password") {
+        passField.type = "name"; // Show password
+    } else {
+        passField.type = "password"; // Hide password
+    }
+}
