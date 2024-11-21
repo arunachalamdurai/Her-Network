9a7023149f7f036eb055556421bfa892c577ea10
ARUNACHALAM A <108666132+arunachalamdurai@users.noreply.github.com>
2024-11-21 19:05:05 +0530

    Delete backend_project directory
    Signed-off-by: ARUNACHALAM A <108666132+arunachalamdurai@users.noreply.github.com>

--- backend_project/login/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import Login_detail
-# Register your models here.
-admin.site.register(Login_detail)
--- backend_project/login/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('login/',views.loginhome,name='login'),
-    path('login/index/',views.indexpage,name='index'),
-]
